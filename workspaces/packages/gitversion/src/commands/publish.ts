ebd148a756885c53c8c244d63925bcfeedfde43e
Joost van der Waal <joost.vanderwaal@aegon.nl>
2024-01-23 21:52:15 +0100

    Getting errors

--- workspaces/packages/gitversion/src/commands/publish.ts
+++ workspaces/packages/gitversion/src/commands/publish.ts
@@ -102,6 +102,7 @@ export class PublishCommand extends GitVersionCommand {
           logger.reportError(`Error during publish. Exit code: ${colorize.redBright(output.exitCode)}`);
         } else if (output.exitCode !== 0) {
           console.log(output.stdout.toString('utf-8'));
+          console.log(output.stderr.toString('utf-8'));
           logger.reportInfo(`Publish error: ${output.exitCode}`);
         } else {
           logger.reportInfo('Publish package success');
