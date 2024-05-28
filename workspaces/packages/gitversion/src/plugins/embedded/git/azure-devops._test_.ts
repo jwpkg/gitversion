6d05d4c4d4466d2c42b019bde5996bee5d86ad38
Joost van der Waal <joost.vanderwaal@aegon.nl>
2024-05-28 16:42:31 +0200

    chroe: Changed unit tests

--- workspaces/packages/gitversion/src/plugins/embedded/git/azure-devops._test_.ts
+++ workspaces/packages/gitversion/src/plugins/embedded/git/azure-devops._test_.ts
@@ -2,16 +2,16 @@ import { AzureDevopsPlugin } from './azure-devops';
 
 describe('Github platform', () => {
   test('Url parsing', () => {
-    expect(AzureDevopsPlugin.parseUrl('https://aegon-nl@dev.azure.com/aegon-nl/apidas/_git/apigee-monorepo')).toStrictEqual({
-      organizationName: 'aegon-nl',
-      projectName: 'apidas',
-      repoName: 'apigee-monorepo',
+    expect(AzureDevopsPlugin.parseUrl('https://username@dev.azure.com/organizationname/projectname/_git/reponame')).toStrictEqual({
+      organizationName: 'organizationname',
+      projectName: 'projectname',
+      repoName: 'reponame',
     });
 
-    expect(AzureDevopsPlugin.parseUrl('git@ssh.dev.azure.com:v3/aegon-nl/apidas/apigee-monorepo')).toStrictEqual({
-      organizationName: 'aegon-nl',
-      projectName: 'apidas',
-      repoName: 'apigee-monorepo',
+    expect(AzureDevopsPlugin.parseUrl('git@ssh.dev.azure.com:v3/organizationname/projectname/reponame')).toStrictEqual({
+      organizationName: 'organizationname',
+      projectName: 'projectname',
+      repoName: 'reponame',
     });
   });
 });
