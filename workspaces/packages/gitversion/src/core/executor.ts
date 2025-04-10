304a92450cf2b1455649fc7df9fa5dfe714ce851
Joost van der Waal <joost.van.der.waal@asr.nl>
2025-04-10 15:14:16 +0200

    fix: Error debugging

--- workspaces/packages/gitversion/src/core/executor.ts
+++ workspaces/packages/gitversion/src/core/executor.ts
@@ -25,8 +25,8 @@ export class Executor implements IExecutor {
       env: process.env,
     });
 
-    console.log('[!!!!DEBUGGING!!!!] exec request', commandAndArgs);
-    console.log('[!!!!DEBUGGING!!!!] exec result', result.exitCode, result.error, result.stdout.toString(), result.stderr.toString());
+    console.log('[!!!!DEBUGGING!!!!] exec request', fullCommand);
+    console.log(`[!!!!DEBUGGING!!!!] exec result c:'${result.exitCode}', e:'${result.error}', stdo:'${result.stdout.toString()}', stde:'${result.stderr.toString()}'`);
 
     if (result.error) {
       this.logError(`${result.error}`, options);
